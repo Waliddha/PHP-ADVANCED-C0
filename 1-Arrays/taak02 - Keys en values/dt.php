bb9a2140319aae436a90639cffee78032666d6f3
Walid Harouche <walid.harouche@student.rocva.nl>
2021-09-30 15:05:54 +0200

    php

---
+++ 1-Arrays/taak02 - Keys en values/dt.php
@@ -0,0 +1,10 @@
+<?php
+
+$persoon["voornaam"]   = "Walid";
+$persoon["achternaam"]   = "Harouche";
+$persoon["leeftijd"]   = "17";
+$persoon["opleiding"]   = "Software devevloper";
+
+
+echo  $persoon['voornaam']. " " .  $persoon['achternaam'] . " Is al jaren bezig met PHP-advanced";
+?>
